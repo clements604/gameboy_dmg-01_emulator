2eb425d7fd44415e7ca6efe72b936d1c59e6deb6
Josh Clements <clements604@gmail.com>
2024-08-24 14:23:56 +1000

    before timer refactor

--- src/timer.rs
+++ src/timer.rs
@@ -1,14 +1,25 @@
+use crate::interupts::Interrupt;
+use crate::CPU::CPU;
+use log::{debug, error, info};
 use std::cell::RefCell;
 use std::rc::Rc;
-use log::{debug, info};
-use crate::CPU::CPU;
-use crate::interupts::Interrupt;
+
+enum TimerFrequency {
+    Hz4096 = 1024,
+    Hz16384 = 256,
+    Hz65536 = 64,
+    Hz262144 = 16,
+}
 
 pub struct Timer {
     div: u16,
-    tima: u16,
-    tma: u16,
-    tac: u16,
+    tima: u8,
+    tma: u8,
+    tac: u8,
+    
+    cycles: u16,
+    enabled: bool,
+    
     cpu: Rc<RefCell<CPU>>,
 }
 
@@ -19,19 +30,23 @@ impl Timer {
             tima: 0,
             tma: 0,
             tac: 0,
+            
+            cycles: 0,
+            enabled: false,
+            
             cpu,
         }
     }
-    pub fn read(&self, address: u16) -> u16 {
+    pub fn read(&self, address: u16) -> u8 {
         match address {
-            0xFF04 => self.div >> 8,
+            0xFF04 => (self.div >> 8) as u8, // TODO does casting to u8 loose the upper bits?
             0xFF05 => self.tima,
             0xFF06 => self.tma,
             0xFF07 => self.tac,
             _ => panic!("Invalid Timer address: {:#X}", address),
         }
     }
-    pub fn write(&mut self, address: u16, value: u16) {
+    pub fn write(&mut self, address: u16, value: u8) {
         match address {
             0xFF04 => self.div = 0,
             0xFF05 => self.tima = value,
@@ -40,27 +55,30 @@ impl Timer {
             _ => panic!("Invalid Timer address: {:#X}", address),
         }
     }
-    pub fn tick(&mut self) {
-        let previous_div = self.div;
-        self.div = self.div.wrapping_add(1);
-        debug!("Previous DIV: {:#X}, New DIV: {:#X}", previous_div, self.div);
+    
+    pub fn cycle(&mut self, cycles: &u16) {
+        for _ in 0..*cycles {
+            let previous_div = self.div;
+            self.div = self.div.wrapping_add(1);
+            debug!("Previous DIV: {:#X}, New DIV: {:#X}", previous_div, self.div);
 
-        let timer_update: bool = match self.tac & 0x3 {
-            0 => (previous_div & (1 << 9) != 0) && (self.div & (1 << 9) == 0),
-            1 => (previous_div & (1 << 3) != 0) && (self.div & (1 << 3) == 0),
-            2 => (previous_div & (1 << 5) != 0) && (self.div & (1 << 5) == 0),
-            3 => (previous_div & (1 << 7) != 0) && (self.div & (1 << 7) == 0),
-            _ => panic!("Invalid Timer frequency: {:#X}", self.tac & 0x3),
-        };
-        debug!("Timer update: {}", timer_update);
-        
-        if timer_update && self.tac & (1<<2) != 0 {
-            self.tima = self.tima.wrapping_add(1);
-            if self.tima == 0xFF {
-                self.tima = self.tma;
-                self.cpu.borrow_mut().trigger_interrupt(Interrupt::TIMER);
+            let timer_update: bool = match self.tac & 0x3 {
+                0 => (previous_div & (1 << 9) != 0) && (self.div & (1 << 9) == 0),
+                1 => (previous_div & (1 << 3) != 0) && (self.div & (1 << 3) == 0),
+                2 => (previous_div & (1 << 5) != 0) && (self.div & (1 << 5) == 0),
+                3 => (previous_div & (1 << 7) != 0) && (self.div & (1 << 7) == 0),
+                _ => panic!("Invalid Timer frequency: {:#X}", self.tac & 0x3),
+            };
+            debug!("Timer update: {}", timer_update);
+
+            if timer_update && self.tac & (1 << 2) != 0 {
+                self.tima = self.tima.wrapping_add(1);
+                if self.tima == 0xFF {
+                    self.tima = self.tma;
+                    self.cpu.borrow_mut().trigger_interrupt(Interrupt::TIMER);
+                }
             }
         }
-
     }
+
 }
